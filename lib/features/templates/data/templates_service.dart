b2a8d9c97992acc60c6e35c96733a64d37cc5e9a
unfae <thefae01@gmail.com>
2025-12-07 02:21:20 +0000

    fixes

--- lib/features/templates/data/templates_service.dart
+++ lib/features/templates/data/templates_service.dart
@@ -6,15 +6,21 @@ import 'template_model.dart';
 
 class TemplatesService {
   Future<List<MagazineTemplate>> fetchTemplates() async {
-    final String url = "https://raw.githubusercontent.com/unfae/MagzineMaker/refs/heads/dev/remote_templates/templates.json";
+    final String url = "https://raw.githubusercontent.com/unfae/MagzineMaker/dev/remote_templates/templates.json";
 
     final response = await http.get(Uri.parse(url));
 
-    if (response.statusCode == 200) {
-      final List<dynamic> data = jsonDecode(response.body);
-      return data.map((e) => MagazineTemplate.fromJson(e)).toList();
-    } else {
-      throw Exception("Failed to load templates");
+    try {
+      final response = await http.get(Uri.parse(templatesUrl));
+      if (response.statusCode == 200) {
+        final List data = jsonDecode(response.body);
+        return data.map((e) => MagazineTemplate.fromJson(e)).toList();
+      } else {
+        throw Exception('Server returned ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to load templates: $e');
     }
+
   }
 }
